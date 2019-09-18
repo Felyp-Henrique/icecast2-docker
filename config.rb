ad1ab6820a5acb9d62734984a6530b7c2f720ab4
plug <plug@felyp@sistemaplug.com.br>
2019-09-18 14:12:03 -0300

    * Made change in Ruby Script to best comprehends of file

--- config.rb
+++ config.rb
@@ -1,36 +1,21 @@
 #!/usr/bin/env ruby
 
+# All variables that config the fields of file icecast.xml are environment variable
+# of shell, then this script only get this variables through ENV hashtable and set 
+# the file /etc/icecast2/icecast.xml using ERB template processor
+
 require 'erb'
 
-$admin = ENV['IC_AUTH_ADMIN']
-$admin_pass = ENV['IC_AUTH_ADMIN_PASS']
-
-$src_pass = ENV['IC_SOURCE_PASS']
-
-$host = ENV['IC_HOSTNAME']
-$port = ENV['IC_PORT']
-
-$num_clients = ENV['IC_LIMITS_CLIENTS']
-$num_sources = ENV['IC_LIMITS_SOURCES']
-
-$queue = ENV['IC_LIMITS_QUEUE_SIZE']
-$burst = ENV['IC_LIMITS_BURST_SIZE']
-
-$cli_timeout = ENV['IC_LIMITS_CLIENT_TIMEOUT']
-$hdr_timeout = ENV['IC_LIMITS_HEADER_TIMEOUT']
-$src_timeout = ENV['IC_LIMITS_SOURCE_TIMEOUT']
-
-# $out variable for save new configuration icecast.xml
-$out = ""
+$icecast_configuration = ""
 
 puts 'Generating the new file /etc/icecast2/icecast.xml'
 
 File.open '/icecast.xml.erb', 'r' do |file|
-  $out << ERB.new(file.read).result
+  $icecast_configuration << ERB.new(file.read).result
 end
 
 puts 'Saving now...'
 
 File.open '/etc/icecast2/icecast.xml', 'w' do |file|
-  file.puts $out
+  file.puts $icecast_configuration
 end
